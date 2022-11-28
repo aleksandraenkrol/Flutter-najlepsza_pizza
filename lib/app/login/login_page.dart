703e4b2f64714de1ecee430f3b3581c68c945d4b
Aleksandra Król <aleksandra.en.28@gmail.com>
2022-11-28 15:46:40 +0100

    Add registration

---
+++ lib/app/login/login_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({
+    Key? key,
+  }) : super(key: key);
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Zaloguj się'),
+              const SizedBox(height: 20),
+              TextField(
+                controller: widget.emailController,
+                decoration: const InputDecoration(hintText: 'E-mail'),
+              ),
+              TextField(
+                controller: widget.passwordController,
+                decoration: const InputDecoration(hintText: 'Hasło'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              Text(errorMessage),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: widget.emailController.text,
+                      password: widget.passwordController.text,
+                    );
+                  } catch (error) {
+                    setState(() {
+                      errorMessage = error.toString();
+                    });
+                  }
+                },
+                child: const Text('Zaloguj się'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
